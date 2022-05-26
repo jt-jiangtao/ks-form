d59922b44bb58a8ca182c2fef14ce5fded010b19
zy-zxcvbnm <1271318085@qq.com>
2022-05-26 15:31:40 +0800

    5.26第一版填写详情

--- src/pages/NewFormResult/index.tsx
+++ src/pages/NewFormResult/index.tsx
@@ -1,5 +1,39 @@
-export default function NewFormResult(){
-    return (
-        <div>NewFormResult</div>
-    )
+import HeaderLayout from "@/layout/HeaderLayout";
+import logo from "@/assets/icon/logo.svg";
+import { useLocation, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import SideBar from "@/pages/NewFormResult/SideBar";
+import "@/styles/NewFormResult/index.scss";
+export default function NewFormResult() {
+  let location = useLocation();
+  let sidebarHash = ["mycreate", "share"];
+  const parseSideBar = () => {
+    if (location.hash.slice(1).length === 0) return "mycreate";
+    else if (sidebarHash.indexOf(location.hash.slice(1)) !== -1)
+      return location.hash.slice(1);
+    return "mycreate";
+  };
+  let [sidebar] = useState(parseSideBar);
+  const navigate = useNavigate();
+  return (
+    <section>
+      <HeaderLayout className="form-list__header">
+        <div className="return" onClick={() => navigate("/new-form-list")}>
+          〈
+        </div>
+        <div className="logo-title">
+          <div className="logo">
+            <img src={logo} />
+            <h1 className="title">这里是上个页面传进来的表的名字</h1>
+          </div>
+        </div>
+      </HeaderLayout>
+      <main className="m">
+        <div className="sideBar">
+          <SideBar active={sidebar} />
+        </div>
+        <div className="result">Result</div>
+      </main>
+    </section>
+  );
 }
